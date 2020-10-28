7695212a78c352ec855ca20c502165a663a6345a
terrencegsu2020 <70604452+terrencegsu2020@users.noreply.github.com>
2020-10-28 14:07:48 -0400

    Add files via upload
    I just created a new user interface for the employee evaluation screen.  This screen collects values from the user such as (eval_id,emp_id, value1,value2,value3,value4,value5, and score)  The value variable stores the result of the question choice response.  Each result has been coded with a range from 0 to 100 depending on a selection form 1 to 5.  Each question has been weighted individually to be use in the evaluation calculation to generate a raw score.    The results submitted on this screen are pass to the evaluationphp file which is the frontend processing of the results of the evaluation screen.  The primary function is to calculate the raw score, Display on the processing screen, and pass the results to the database.

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title>Home</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="./resources/stylesheet.css">
+    </head>
+    
+    <body>
+        <div class= banner>- EMPLOYEE EVALUATION SYSTEM -</div>
+        
+        <div class= wrapper>
+        
+        <div id= stock> </div>
+        
+        <div class = content>
+            <a href= "./pageunderconstruction.html">
+            <div class= menu id= top_left>
+                Search Employee
+            </div>
+            </a>
+            
+            <a href= "./newevaluation.php">
+            <div class= menu id= top_right>
+                Create Evaluation
+            </div>
+            </a>
+            
+            <a href= "./newemployee.php">
+            <div class= menu id= bottom_left>
+                Add Employee
+            </div>
+            </a>
+            
+            <a href= "./newdepartment.php">
+            <div class= menu id= bottom_right>
+                Create Department
+            </div>
+            </a>
+			
+			<a href= "./pageunderconstruction.html">
+            <div class= menu id= bottom_right>
+                View Employee Bonus
+            </div>
+            </a>
+        </div>
+        
+        <div id= copyright>
+            The Techonauts: Bonnie Atelsek - Matthew Kabat - Nathan Murzello - Terrence Gaines
+        </div>
+        
+        </div>
+    </body>
+</html>
---
+++ newevaluationphp.php
@@ -0,0 +1,77 @@
+<html>
+
+  <head>
+    <title>New Evaluation</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="./resources/construction.css">
+  </head>
+  
+  <body>
+    <?php
+	
+    $servername = "PANTHERT";
+    $username = "batelsek1";
+    $password = "batelsek1";
+    
+    // Create connection
+	
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+      echo "Operation unsuccessful: Employee was not added, please try again.<br>";
+      die("Connection failed: " . $conn->connect_error);
+    }
+	
+	$weight1 = .30;   ##criteria question 1 (30%)
+	$weight2 = .15;   ##criteria question 2 (15%)
+	$weight3 = .40;   ##criteria question 3 (40%)
+	$weight4 = .10;   ##criteria question 4 (10%)
+	$weight5 = .05;   ##criteria question 5 (5%)
+    $approved = 0;
+    
+	$emp_id = $_POST['emp_id'];
+    $eval_id = $_POST['eval_id'];
+    $value1 = $_POST['value1'];
+	$value2 = $_POST['value2'];
+	$value3 = $_POST['value3'];
+	$value4 = $_POST['value4'];
+	$value5 = $_POST['value5'];
+	
+	
+	//calculate the evaluation raw score
+    $score = (($value1*$weight1) + ($value2*$weight2) + ($value3*$weight3) + ($value4*$weight4) + ($value5*$weight5));
+    //print "<div>Evaluation results are:  $score </div>";
+	echo  "$emp_id, Evaluation raw score results are:, $score","%";
+
+    if ($approved == 2) {
+      echo "Evaluation successfully added.";
+      $sql = "INSERT INTO evaluation(score)
+      VALUES (‘$score’)";
+      
+      if ($conn->query($sql) === TRUE) {
+        echo "New evaluation added successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+    }
+    else {
+      echo "Operation unsuccessful: Evaluation was not added, please try again.";
+	  echo "Evaluation results are:, $score";
+    }
+
+    ?>
+
+    <div class = banner>
+      <a href= "./homepage.html"> Back </a>
+    </div>
+        
+    <div id= copyright>
+      The Techonauts: Bonnie Atelsek - Matthew Kabat - Nathan Murzello - Terrence Gaines
+    </div>
+    
+    <?php
+      $conn->close();
+    ?>
+  </body>
+
+</html>
