16d8e08fd480697f10b2ae3d4a43347827beb933
Nathan Murzello <nathanthekyle@gmail.com>
2020-11-01 17:51:01 -0500

    Reorganized
    Sprint 4

--- current_Project/newevaluationphp.php
+++ current_Project/newevaluationphp.php
@@ -8,13 +8,15 @@
   
   <body>
     <?php
-
-    require 'vendor/autoload.php';
-    $client = new MongoDB\Client(
-        'mongodb+srv://techno:techno123@cluster0.k9zfj.mongodb.net/EmployeeSystem?retryWrites=true&w=majority');
-    
-    $collection=$client->selectCollection('EmployeeSystem','Evaluation');
 	
+	
+	
+  require 'vendor/autoload.php';
+  $client = new MongoDB\Client(
+      'mongodb+srv://techno:techno123@cluster0.k9zfj.mongodb.net/EmployeeSystem?retryWrites=true&w=majority');
+  
+  $collection=$client->selectCollection('EmployeeSystem','Evaluation');
+
 	$weight1 = .30;   ##criteria question 1 (30%)
 	$weight2 = .15;   ##criteria question 2 (15%)
 	$weight3 = .40;   ##criteria question 3 (40%)
@@ -23,8 +25,8 @@
     $approved = 0;
     
 	$emp_id = $_POST['emp_id'];
-  $eval_id = $_POST['eval_id'];
-  $value1 = $_POST['value1'];
+    $eval_id = $_POST['eval_id'];
+    $value1 = $_POST['value1'];
 	$value2 = $_POST['value2'];
 	$value3 = $_POST['value3'];
 	$value4 = $_POST['value4'];
@@ -46,26 +48,28 @@
 	//calculate the evaluation raw score
     $score = (($value1*$weight1) + ($value2*$weight2) + ($value3*$weight3) + ($value4*$weight4) + ($value5*$weight5));
     //print "<div>Evaluation results are:  $score </div>";
-	echo  "$emp_id, Evaluation raw score results are:, $score","% ";
+	//echo  "$emp_id, Evaluation raw score results are:, $score","%";
 
-    if ($approved == 2) {
-      echo "Evaluation successfully added.";
-      $num_emp_id=(int)$_POST['emp_id'];
-      $num_eval_id=(int)$_POST['eval_id'];
-      $num_score=(double)$score;
-      $insert=array(
-          '_id'=>$num_eval_id,
-          'employee id'=>$num_emp_id,
-          'eval_score'=>$num_score
-      );
+  if ($approved == 2) {
+    echo "Evaluation successfully added.";
+    $num_emp_id=(int)$_POST['emp_id'];
+    $num_eval_id=(int)$_POST['eval_id'];
+    $num_score=(double)$score;
+    $insert=array(
+        '_id'=>$num_eval_id,
+        'employee id'=>$num_emp_id,
+        'eval_score'=>$num_score
+    );
 
-      $collection->insertOne($insert);
-    }
-    else {
-      echo "Operation unsuccessful: Evaluation was not added, please try again.";
-	  echo "Evaluation results are:, $score";
-    }
-
+    $collection->insertOne($insert);
+  }
+  else {
+    echo "Operation unsuccessful: Evaluation was not added, please try again.";
+  echo "Evaluation results are:, $score";
+  }
+  
+echo "New evaluation added successfully";
+echo "Evaluation results are:, $score";
     ?>
 
     <div class = banner>
@@ -76,6 +80,9 @@
       The Techonauts: Bonnie Atelsek - Matthew Kabat - Nathan Murzello - Terrence Gaines
     </div>
     
+    <?php
+     
+    ?>
   </body>
 
 </html>
